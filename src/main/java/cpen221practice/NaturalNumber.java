0096968028161206047f6302956ee86d045987dc
[Nokia] Methasit Tantiplubtong <nokia_tuk@hotmail.com>
2022-10-21 09:28:08 -0700

    practice 3 done

--- src/main/java/cpen221practice/NaturalNumber.java
+++ src/main/java/cpen221practice/NaturalNumber.java
@@ -1,7 +1,33 @@
 package cpen221practice;
 
 class NaturalNumber {
+    private int num;
 
+    public NaturalNumber (int n){
+        if(n <= 0) {
+            throw new IllegalArgumentException("You must supply a natural number (positive integer)");
+        }
+        this.num = n;
+    }
 
+    public Classification getClassification(){
+        int sum = 0;
+
+        for(int i = 1; i < Math.sqrt((double) num); i++){
+            if(this.num % i == 0){
+                sum += i + this.num/i;
+            }
+        }
+
+        sum -= this.num;
+
+        if(sum == num){
+            return Classification.PERFECT;
+        } else if( sum > num){
+            return Classification.ABUNDANT;
+        } else{
+            return Classification.DEFICIENT;
+        }
+    }
 
 }
